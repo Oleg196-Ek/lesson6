904f525e87f50d2bd1960fc04f25a81150ff2922
Oleg196-Ek <zhookov@mail.ru>
2024-09-15 20:07:24 +0300

    Add files via upload

---
+++ module_1.6.py
@@ -0,0 +1,19 @@
+# my_dict = {'Stas': 1975, 'Alex': 1970, 'Slava': 1983}
+# print(my_dict)
+# print(my_dict['Stas'])
+# my_dict['Oleg'] = 1971
+# print(my_dict)
+# my_dict.update({'Vika': 2009, 'Pavel': 2006})
+# print(my_dict)
+# del my_dict['Alex']
+# print(my_dict.get('Alex'))
+# print(my_dict)
+my_set = {1, 2, 3, 4, 1, 2, 3, 4, 'Pavel','Pavel',(1,2,3,4),True}
+print(my_set)
+my_set.add(5)
+print(my_set)
+my_set.add('Slava')
+print(my_set)
+my_set.remove(4)
+print(my_set)
+
